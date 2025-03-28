b5b22697a2c4978b3671754f15e5dd93aceb2cce
Kate3121 <145933961+Kate3121@users.noreply.github.com>
2025-03-28 13:06:59 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAplication_Stetsenko_K22_3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
